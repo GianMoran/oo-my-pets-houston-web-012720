ac81905e5ce9bb32aad4609e743ab3c286b93601
Gian Moran <gpmoran_@outlook.com>
2020-02-01 19:39:02 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
+  attr_accessor :owner
+  attr_reader :name 
+   @@all = []
+ def initialize(name,owner)
+   @name = name 
+   @owner = owner 
+   @@all << self
+ end
 end

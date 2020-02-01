b99a2f1757ea04380fb4b312d435de191d2d24c3
Gian Moran <gpmoran_@outlook.com>
2020-02-01 20:42:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,6 +56,6 @@ class Owner
      end
    end
    def list_pets 
-     "I have #{self.dogs.count} and #{self.cats.count} "
+     "I have #{self.dogs.count} dogs and #{self.cats.count} cats "
    end
 end
